b8766b60bc64e698ce30edcbd8e846747ad4af25
Brutlex <miljevic.bojan@yahoo.com>
2016-11-28 16:01:42 +0100

    Index + Tags 1.0.0.1

--- public_html/tags/header.php
+++ public_html/tags/header.php
@@ -1,17 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="Online marketplace for used bicycles">
+<meta name="author" content="Haris Mahalbasic, Bojan Miljevic">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
-<head>
-
-    <?php include 'tags\header.php'; ?>
-
-    <title>Welcome to BikesForYou</title>
-
-</head>
-
-<body>
-<?php include 'tags\navbar.php'; ?>
-
-</body>
-
-</html>
