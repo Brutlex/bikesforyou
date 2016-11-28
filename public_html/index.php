3935da91c665b5f3d4e1a28390657d8b0e1253ce
Brutlex <miljevic.bojan@yahoo.com>
2016-11-28 15:54:13 +0100

    Index + Tags 1.0

---
+++ public_html/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <?php include 'tags\header.php'; ?>
+
+    <title>Welcome to BikesForYou</title>
+
+</head>
+
+<body>
+<?php include 'tags\navbar.php'; ?>
+
+</body>
+
+</html>
